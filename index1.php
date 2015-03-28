2a8c104d66d97e114525ab13a306f86cc97735b8
SHASHI SHEKHAR <shashibarnwaldav@gmail.com>
2015-03-28 22:49:22 +0530

    new index

--- index1.php
+++ index1.php
@@ -3,13 +3,13 @@
 <style>
 html,body{height:100%}
 .wrap{width:100%;height:100%;position:relative}
-.head{height:100px;top:0;left:0;width:100%} 
+.head{height:100px;position:fixed;top:0;left:0;width:100%} 
 .bodywrap{margin-top:100px;width:100%;}
 .left,.right{width:20%;height:100%;position: fixed;}
 .left,.center,.right,.bodywrap{height:100%}
 .left{background-color:#c3dfef;float:left;margin-left: 0px;width:20%;}
 .right{background-color:#c3dfef;float:right;margin-left: 80%;width:20%;}
-.center{background-color:white;margin-left:200px; overflow:scroll; overflow-x:hidden;overflow-y:hidden;margin-left:20%;width:60%;}
+.center{background-color:white;margin-left:200px; overflow:scroll; overflow-x:hidden;margin-left:20%;width:60%;}
 .head{background-color:#777}
 </style>
 
@@ -20,10 +20,10 @@ error_reporting(~E_NOTICE & ~E_DEPRECATED);
 ?>
 <div class="wrap">
 
-<div class="head" style="position: fixed; width: 100%;height:10%; background-color: white;margin-top: 1px;"><?php include('./mainheader.php');?></div>
+<div class="head" style="position: fixed; width: 100%;height:13%; background-color: white;margin-top: 1px;"><?php include('./mainheader.php');?></div>
 <div class='bodywrap'>
-        <div class='left' style="position: fixed;">
-<ul style="margin-top: 70%;">
+        <div class='left'>
+<ul >
 <li><a href="html/question.html">Post Questions</a></li>
 <li><a href="php/answer.php">Post Answer</a></li>
 <li><a href="php/preview.php">Preview Question and Answer post!!</a></li>
