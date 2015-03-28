c657cec91731e440884cd09dec8b7b0244fb3ab9
mohitbhagwani94 <mohitbhagwani94@gmail.com>
2015-03-28 22:05:22 +0530

    finally all sqli fixed

--- php/answer.php
+++ php/answer.php
@@ -2,24 +2,18 @@
 session_start();
 error_reporting(~E_NOTICE & ~E_DEPRECATED);
 ?>
-<?php include('header2.php');?>
+<?php include('header.php');?>
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "iecsis";
-
-$conn =  mysql_connect($servername, $username, $password);
-mysql_select_db($dbname);
+require_once 'connection.php';
 $query="select * from tb_question order by question_post_date desc";
-$result=mysql_query($query);
+$result=mysqli_query($connection,$query);
 echo "<center><table class='tbl1'><tr class='xx'><td>Questions Asked:</td><td>Question Id</td><td>User Id</td><td>Answered Bit</td><td>Post Date</td></tr>";
 
 $queno=1;
-while($res=mysql_fetch_row($result))
+while($res=mysqli_fetch_assoc($result))
 {
 
-echo "<tr><td>Q".$queno." ..".$res[0]."</td><td>".$res[1]."</td> <td>".$res[2]."</td> <td>".$res[3]."</td> <td>".$res[4]."</td> </tr><br>";
+echo "<tr><td>Q".$queno." ..".$res['question']."</td><td>".$res['question_id']."</td> <td>".$res['user_id']."</td> <td>".$res['answer_bit']."</td> <td>".$res['question_post_date']."</td> </tr><br>";
 echo "<tr><td><form action='answerpost.php' method='get'>  </td><td><a href=answerpost.php?qid=$res[1]&uid=$res[2]>".
 "<input class='btn' type=button value='Answer this Question' name=send /> </a>".
 "</form></td></tr>"; 
--- php/header.php
+++ php/header.php
@@ -56,12 +56,12 @@ error_reporting( ~E_NOTICE & ~E_DEPRECATED);
                 <ul class="nav navbar-nav navbar-right pull-left"> 
 
                     <a class="navbar-brand" href="#">Iecsis</a>
-                    <li class="nohover list-unstyled"><a href="./index.php" >
+                    <li class="nohover list-unstyled"><a href="../index.php" >
                    		<img  src="../images/logo.png" style="margin: -15px 0px -10px 0px;"></img></a>
                     </li>
 
 
-                    <li class="active"><a href="./index.php" >
+                    <li class="active"><a href="../index.php" >
                     <span  class="glyphicon glyphicon-home"></span>Home</a></li>
 
                     
