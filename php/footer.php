b20f27b63a3f914871512f0c15f2f544481a9710
SHASHI SHEKHAR <shashibarnwaldav@gmail.com>
2015-03-31 00:09:34 +0530

    remove google and bootstrap link

--- php/footer.php
+++ php/footer.php
@@ -1,7 +1,21 @@
-<div style="width:100%;height:25%;background-color:gray;position: relative; opacity:0.6">
-<center>
-<p>&copy 2015 IECSIS</p>
-</center>
+<div style="width:100%;height:25%;background-color:#e2e2e2;position: relative; opacity:0.">
+<!-- Social Footer, Colour Matching Icons -->
+<!-- Include Font Awesome Stylesheet in Header -->
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<!-- // -->
+<div class="container">
+        <div class="text-center center-block"></div>
+           <center> <br />
+                <a href="https://www.facebook.com/bootsnipp"><i id="social" class="fa fa-facebook-square fa-3x social-fb"></i></a>
+	            <a href="https://twitter.com/bootsnipp"><i id="social" class="fa fa-twitter-square fa-3x social-tw"></i></a>
+	            <a href="https://plus.google.com/+Bootsnipp-page"><i id="social" class="fa fa-google-plus-square fa-3x social-gp"></i></a>
+	            <a href="mailto:bootsnipp@gmail.com"><i id="social" class="fa fa-envelope-square fa-3x social-em"></i></a>
+                </center>
+</div>
+    
+</div>
 
+
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 </div>
 </html>
