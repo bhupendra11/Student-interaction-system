47de7f96da0d59c1511cd8eb751139aff2ec8184
bhupendra11 <bhupendrashekhawat1108@gmail.com>
2015-03-31 01:08:12 +0530

    modified answerpost.php, login.php, register.php and working on update.php

--- html/login.php
+++ html/login.php
@@ -29,13 +29,13 @@
 <body>
 	
 <?php include('../php/header.php');?>
+
+<!-- login form coding-->
+<!--
 <div class="container">
-	<center>
-	<h2 style="color:black;">
-		Login Form
-	</h2>
-	<!-- login form coding-->
-	<table class="formtbl">
+	
+	
+	  <table class="formtbl">
 		<form action="../html/login.php" method="POST">
 		<tr>
 			<td style="color:black; ">Email</td>
@@ -50,10 +50,63 @@
 			<td><input class='btn' type="submit" value="Login" name="Login"></input></td>
 		</tr>
 		</form>
-	</table>
-	<!--login coding ended-->
-	</center>
+	</table>    
+	
+</div>	-->
+
+<!--login coding ended-->
+
+<div class='container'>
+<center>
+</br>
+	<h2 style="color:black;">
+		Login 
+	</h2>
+</center></br></br>	
+
+
+
+<form class="form-horizontal" action="../html/login.php" method="POST">
+    <div class="form-group">
+        <label for="inputEmail" class="control-label col-lg-offset-2 col-lg-2">Email</label>
+        <div class="col-lg-4">
+            <input name='email' type="email" class="form-control" id="inputEmail" placeholder="Email">
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="inputPassword" class="control-label col-lg-offset-2 col-lg-2">Password</label>
+        <div class="col-lg-4">
+            <input name='password' type="password" class="form-control" id="inputPassword" placeholder="Password">
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-lg-offset-4 col-lg-8">
+            <div class="checkbox">
+                <label><input type="checkbox"> Remember me</label>
+            </div>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-lg-offset-4 col-lg-6">
+            <button type="submit" class="btn btn-primary" value="Login" name="Login" style='padding:3px;'>Login</button>
+        </div>
+    </div>
+</form>
+
 </div>
+
+
+
+
+
+
+
+
+
+
+
+	
+
 	<div id="foot" style="clear:both;">
 	  <center> <B>CONTACT US</B></center>
 	</div>
