31d27dd41c41c3430c875dedecd17ea26823819c
ashishjangu <ashish.jangu@gmail.com>
2015-03-30 15:16:29 -0400

    index.php updated(footerupdate)

--- php/footer.php
+++ php/footer.php
@@ -6,7 +6,10 @@
 <div class="container">
         <div class="text-center center-block"></div>
            <center> <br />
-                <a href="https://www.facebook.com/bootsnipp"><i id="social" class="fa fa-facebook-square fa-3x social-fb"></i></a>
+<center><a href="#">Advertise</a>|<a href="#">Contact</a>|<a href="#">FAQ</a>|<a href="#">Privacy Policy</a>|<a href="#">Terms of Use</a>|<a href="#">Translate</a>
+
+<center>©2015-2020 iecsis.studenthelp@gmail.com, All Rights Reserved.</center>               
+			   <a href="https://www.facebook.com/bootsnipp"><i id="social" class="fa fa-facebook-square fa-3x social-fb"></i></a>
 	            <a href="https://twitter.com/bootsnipp"><i id="social" class="fa fa-twitter-square fa-3x social-tw"></i></a>
 	            <a href="https://plus.google.com/+Bootsnipp-page"><i id="social" class="fa fa-google-plus-square fa-3x social-gp"></i></a>
 	            <a href="mailto:bootsnipp@gmail.com"><i id="social" class="fa fa-envelope-square fa-3x social-em"></i></a>
